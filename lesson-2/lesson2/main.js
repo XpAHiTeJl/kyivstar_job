e8771db858c2b0a467598ef5cc62dd74167f678b
XpAHiTeJl <robengud2@gmail.com>
2024-02-10 23:53:45 +0200

    localstorage

--- lesson-2/lesson2/main.js
+++ lesson-2/lesson2/main.js
@@ -30,3 +30,21 @@ stopButton.addEventListener("click", () => {
   video.pause();
   pauseButton.textContent = "Play";
 });
+
+function LocalStorageManager() {}
+
+LocalStorageManager.prototype.saveData = function (key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+LocalStorageManager.prototype.getData = function (key) {
+  const data = localStorage.getItem(key);
+  return JSON.parse(data);
+};
+
+const localStorageManager = new LocalStorageManager();
+
+localStorageManager.saveData("user", { name: "John", age: 30 });
+
+const userData = localStorageManager.getData("user");
+console.log(userData);
