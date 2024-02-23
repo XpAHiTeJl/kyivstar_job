73b69a11daafe77fee1dcdbf910ff083e715eed3
XpAHiTeJl <robengud2@gmail.com>
2024-02-23 22:32:01 +0200

    fix-button

--- lesson-3/main.js
+++ lesson-3/main.js
@@ -4,6 +4,5 @@ const burgerMenu = document.getElementById("burger");
 const menu = document.getElementById("menu");
 
 burgerMenu.addEventListener("click", () => {
-  burgerMenu.classList.toggle("active");
   menu.classList.toggle("active");
 });
