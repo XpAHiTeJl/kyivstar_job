69cad162bc13520621ff9521815aea0e9cc018fa
XpAHiTeJl <robengud2@gmail.com>
2024-04-12 00:25:37 +0300

    menu

--- lesson-8/src/components/Nav.jsx
+++ lesson-8/src/components/Nav.jsx
@@ -10,7 +10,8 @@ const ListItems = [
 export default function Nav() {
   return (
     <nav className="container p-0  bg-image" style={{ height: "700px" }}>
-      <ul className="container d-flex justify-content-around w-50 align-items-center list-unstyled ms-5 mt-4 ">
+      <ul className="headermenu container d-flex justify-content-around w-50 align-items-center list-unstyled ms-5 mt-4 ">
+        <i class="fa-solid fa-bars" id="burger"></i>
         <div className="logo text-light">
           <h3>room</h3>
         </div>
