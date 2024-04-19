bbb251452be0095cdafd9fc157490dda3ed80cdf
XpAHiTeJl <robengud2@gmail.com>
2024-04-20 00:28:26 +0300

    fix-prew-problem

--- lesson-9/src/components/Nav.jsx
+++ lesson-9/src/components/Nav.jsx
@@ -14,10 +14,12 @@ export default function Nav({ type }) {
       style={{ height: "700px" }}
     >
       <ul className="headermenu container d-flex justify-content-around w-50 align-items-center list-unstyled ms-5 mt-4 ">
-        <i class="fa-solid fa-bars" id="burger"></i>
-        <div className="logo text-light">
-          <h3>room</h3>
-        </div>
+        <li>
+          <i class="fa-solid fa-bars" id="burger"></i>
+          <div className="logo text-light">
+            <h3>room</h3>
+          </div>
+        </li>
         {ListItems.map(({ name, url }) => (
           <li key={name}>
             <a className="navlink text-light" href={url}>
