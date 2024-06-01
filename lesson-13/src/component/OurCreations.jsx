b58a76b3a4a5a1e4a188e3f2ea12ad66d3c441f4
XpAHiTeJl <robengud2@gmail.com>
2024-06-02 01:03:57 +0300

    photoMobile

--- lesson-13/src/component/OurCreations.jsx
+++ lesson-13/src/component/OurCreations.jsx
@@ -10,8 +10,14 @@ import pocketborealis from "./../img/image-pocket-borealis.jpg";
 import thecuriosity from "./../img/image-curiosity.jpg";
 import makeitfisheye from "./../img/image-fisheye.jpg";
 
+import deepEarthImgMobile from "./../imgMobile/image-deep-earth.jpg";
+
 const data = [
-  { name: "deep earth", imgSrc: deepEarthImg },
+  {
+    name: "deep earth",
+    imgSrc: deepEarthImg,
+    imgSrcMobile: deepEarthImgMobile,
+  },
   { name: "night arcade", imgSrc: nightarcade },
   { name: "soccer team vr", imgSrc: soccerteamvr },
   { name: "the grid", imgSrc: thegrid },
@@ -27,13 +33,16 @@ export default function OurCreations() {
       {data.map((item, index) => (
         <div
           key={index}
-          className="w-4/6 rounded overflow-hidden shadow-lg relative group "
+          className=" lg:w-4/6 rounded overflow-hidden shadow-lg relative group "
         >
-          <img
-            className="group-hover:opacity-50 transition duration-300 ease-in-out"
-            src={item.imgSrc}
-            alt={item.name}
-          />
+          <picture>
+            <source media="(max-width: 767px)" srcSet={item.imgSrcMobile} />
+            <img
+              className="group-hover:opacity-50 transition duration-300 ease-in-out"
+              src={item.imgSrc}
+              alt={item.name}
+            />
+          </picture>
           <div className="absolute inset-0 bg-gradient-to-t from-gray-700 via-transparent to-transparent"></div>
           <span className="uppercase absolute text-xl mb-5 bottom-0 left-7 w-28	 text-white group-hover:text-black transition duration-300 ease-in-out">
             {item.name}
